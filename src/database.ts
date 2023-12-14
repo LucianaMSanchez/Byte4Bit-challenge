6d1effa2375a7515a9353a87e889c804992b6175
LucianaMSanchez <lulutcht@hotmail.com>
2023-12-14 01:04:49 -0300

    adding mongo atlas

--- src/database.ts
+++ src/database.ts
@@ -3,7 +3,7 @@ import config from "./config";
 
 export const startConnection = async () => {
     try {
-        const db = await mongoose.connect(`mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`)
+        const db = await mongoose.connect(config.MONGO_HOST)
         console.log("Database connected to :", db.connection.name)
     } catch (error) {
         console.error(error)
