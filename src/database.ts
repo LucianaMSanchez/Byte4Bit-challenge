83735b1ba33e6834e6b7b46e1255ba348e088621
LucianaMSanchez <lulutcht@hotmail.com>
2023-12-14 01:22:06 -0300

    deploy settings

--- src/database.ts
+++ src/database.ts
@@ -3,7 +3,7 @@ import config from "./config";
 
 export const startConnection = async () => {
     try {
-        const db = await mongoose.connect(config.MONGO_HOST as string)
+        const db = await mongoose.connect(config.MONGO_HOST)
         console.log("Database connected")
     } catch (error) {
         console.error(error)
